6d4b36d00814307b016033045a3bfbb6308dd5bf
stw8194 <stw8194@gmail.com>
2025-03-27 16:24:37 +0900

    feat: Add logged-out-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { LoggedOutRouter } from "./routers/logged-out-router";
+import { LoggedInRouter } from "./routers/logged-in-router";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
 
 function App() {
-  return <LoggedOutRouter />;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,1 +1,18 @@
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login } from "../pages/login";
+import { CreateAccount } from "../pages/create-account";
+
+export const LoggedOutRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path={"/create-account"}>
+          <CreateAccount />
+        </Route>
+        <Route path={"/"}>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
