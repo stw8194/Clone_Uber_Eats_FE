22256296708682bedb3ab8f13016cec5c22a844f
stw8194 <stw8194@gmail.com>
2025-04-02 22:59:40 +0900

    style: modify login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,8 @@ import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
 import { useMutation } from "@apollo/client";
 import { graphql } from "../gql";
+import logo from "../images/logo.svg";
 import { LoginMutation, LoginMutationVariables } from "../gql/graphql";
-import { watch } from "fs";
 
 const LOGIN_MUTATION = graphql(`
   mutation Login($loginInput: LoginInput!) {
@@ -56,12 +56,13 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-        <h3 className="text-3xl text-gray-800">Log In</h3>
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={logo} alt="" className="w-52 mb-5" />
+        <h4 className="w-full text-left text-3xl mb-10">Welcome back</h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5  w-full"
         >
           <input
             {...register("email", { required: "Email is required" })}
