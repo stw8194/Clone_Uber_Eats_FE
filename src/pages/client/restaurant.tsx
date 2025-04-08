f9fb6b09f864b62f3027650f71e00e47d4f2b80c
stw8194 <stw8194@gmail.com>
2025-04-08 18:24:42 +0900

    feat: restaurants list

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -59,17 +59,30 @@ export const Restaurants = () => {
         />
       </form>
       {!loading && (
-        <div className="max-w-screen mx-auto mt-8">
+        <div className="max-w-screen-xl mx-auto mt-8">
           <div className="flex justify-around max-w-sm mx-auto">
             {data?.allCategories.categories?.map((category) => (
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col group items-center cursor-pointer">
                 <div
-                  className="w-14 h-14 bg-cover rounded-full cursor-pointer"
+                  className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
                   style={{ backgroundImage: `url(${category.coverImg})` }}
                 ></div>
                 <span className="mt-1 text-sm font-medium">
                   {category.name}
                 </span>
+              </div>
+            ))}
+          </div>
+          <div className="grid mt-10 grid-cols-4 gap-x-4 gap-y-8">
+            {data?.restaurants.results?.map((restaurant) => (
+              <div>
+                <div
+                  className="bg-cover bg-center mb-2 rounded-xl py-16"
+                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
+                ></div>
+                <h3 className="text-xl font-medium cursor-pointer">
+                  {restaurant.name}
+                </h3>
               </div>
             ))}
           </div>
