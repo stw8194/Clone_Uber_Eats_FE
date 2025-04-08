2ee595d351b20e2de1cd300653ef8158dd00e238
stw8194 <stw8194@gmail.com>
2025-04-08 17:22:13 +0900

    feat: show all categories

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,1 +1,80 @@
-export const Restaurants = () => <h1>Restaurants</h1>;
+import { useQuery } from "@apollo/client";
+import { graphql } from "../../gql";
+import {
+  RestaurantsPageQuery,
+  RestaurantsPageQueryVariables,
+} from "../../gql/graphql";
+
+const RESTAURANTS_QUERY = graphql(`
+  query RestaurantsPage($restaurantsInput: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+
+    restaurants(input: $restaurantsInput) {
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`);
+
+export const Restaurants = () => {
+  const { data, loading } = useQuery<
+    RestaurantsPageQuery,
+    RestaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      restaurantsInput: {
+        page: 1,
+        limit: 10,
+      },
+    },
+  });
+
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 bg-white w-1/4"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex flex-col items-center">
+                <div
+                  className="w-14 h-14 bg-cover rounded-full cursor-pointer"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
