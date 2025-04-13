ff258e787c6d6568dfdd9e2e7b9a70789f2b58f8
stw8194 <stw8194@gmail.com>
2025-04-13 15:16:03 +0900

    test: show more button

---
+++ src/components/__test__/showmore-button.spec.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { ShowMoreButton } from "../showmore-button";
+
+const onClick = jest.fn();
+
+describe("ShowMoreButton", () => {
+  it("should render OK with props", () => {
+    render(<ShowMoreButton page={1} totalPages={2} onClick={onClick} />);
+    expect(screen.getByText("Show more")).toBeInTheDocument();
+  });
+
+  it("should not show the load more button if no data remains", () => {
+    render(<ShowMoreButton page={1} totalPages={1} onClick={onClick} />);
+    expect(screen.queryByText("Show more")).not.toBeInTheDocument();
+  });
+});
