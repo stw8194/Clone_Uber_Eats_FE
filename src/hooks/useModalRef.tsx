532ceb6a56b06a428d46efee532d324634ae0b90
stw8194 <stw8194@gmail.com>
2025-05-07 18:32:23 +0900

    refactor: Make modalref into hooks

---
+++ src/hooks/useModalRef.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export const useModalRef = (
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return modalRef;
+};
